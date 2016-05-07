41cc8f7069af1d184f61aa21c9e51a8b6cc45617
mickybart <mickybart@pygoscelis.org>
2016-05-07 19:32:35 +0200

    Archlinux: schroot-hybris
    Move from schroot-android to schroot-hybris
    
    Add an unified patch to support Android environment setting.
    That permit to set LD_LIBRARY_PATH, PATH, PS1, ANDROID_ROOT, LD_PRELOAD into the chroot.
    The home of the user is always set to / as Android is not compliant with LHS to
    avoid directory change issue.
    
    So it is now possible to run a command directly.
    eg: schroot -r -c droid -- ls -l /
    
    Packaging for hybris-device doesn't need anymore to use a static busybox for sh and
    to create a root folder with profile that export environment. Of course you need to
    set 'android-environment=true' into the schroot configuration.

--- Android/android_hybris/packaging/Android.mk
+++ Android/android_hybris/packaging/Android.mk
@@ -7,7 +7,6 @@ include $(CLEAR_VARS)
 #
 # Provide:
 #   /system
-#     + static busybox for sh
 #   root (boot ramdisk)
 #   headers (at least for libhybris compilation)
 #   boot.img (kernel + hyrbis ramdisk)
@@ -20,9 +19,6 @@ hybris-device: $(DEFAULT_GOAL) $(BUILT_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/
 	$(hide) rm -rf $(PRODUCT_OUT)/headers
 	$(hide) ./hybris/packaging/extract-headers.sh $(TOP) $(PRODUCT_OUT)/headers $(shell (echo $(PLATFORM_VERSION) | tr '.' ' '))
 	$(call pretty,"Creating package: $@.tgz")
-	$(hide) cp -f $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/system/xbin/busybox-static
-	$(hide) rm -f $(PRODUCT_OUT)/system/xbin/sh
-	$(hide) ln -s busybox-static $(PRODUCT_OUT)/system/xbin/sh
 	$(hide) rm -f $(PRODUCT_OUT)/$@.tgz
 	$(hide) tar -czf $(PRODUCT_OUT)/$@.tgz $(PRODUCT_OUT)/system $(PRODUCT_OUT)/headers $(PRODUCT_OUT)/root
 	$(call pretty,"Package created: $@.tgz")
