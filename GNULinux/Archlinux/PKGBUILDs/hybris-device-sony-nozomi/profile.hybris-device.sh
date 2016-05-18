640e82f61bbfdd57e7fd9e7c42849a2e4a43d404
mickybart <mickybart@pygoscelis.org>
2016-05-18 14:44:47 -0400

    Archlinux: hybris-ready, schroot-hybris and hybris-device-sony-nozomi (sdroid, export, systemd integration, android init ...)
    PKGBUILD: hybris-device-sony-nozomi:
    
    review PKGBUILD compliance
    fstab is not flushed anymore due to Android /init patch that will assume that every filesystem are already mounted and uncrypted (do_mount_all).
    /etc/default/hybris-device is now fully compatible with systemd EvironmentFile directive (eg: hybris-ready-bootanim.service)
    /etc/profile.d/hybris-device.sh will export useful variables (eg: permit to avoid -platform argument for Qt applications).
    
    PKGBUILD: schroot-hybris:
    
    new PATH variable setting to match Android 5 (/init.environ.rc)
    split package into 2 packages named schroot-hybris and schroot-droid.
    
    schroot-droid permit to easily manage an Android schroot session with sdroid command.
    If the Android schroot session is not started, it will do it for you.
    
    Example:
    
    Without sdroid:
    schroot -b -c android -n droid
    schroot -r -c droid -- ls -l /
    schroot -r -c droid
    schroot -e -c droid
    
    With sdroid:
    sdroid ls -l /
    sdroid
    sdroid -e
    
    sdroid will restart a session if the session was not cleaned properly after a reboot.
    sdroid will automatically execute itself to the cgroup systemd/system.slice/hybris-ready.service if the cgroup exist and to permit a better tracking
    of Android running processes by systemd when you are using hybris-ready.service (see hybris-ready package)
    
    PKGBUILD: hybris-ready:
    
    Include multiple packages but the main purpose is to fully support hybris system.
    
    android-init command permit to manage Android /init process. This can be managed with systemd unit file hybris-ready.service
    If enabled, this service is part of the basic.target which is a default dependency for every units (except those with DefaultDependencies=no)
    hybris-ready.service can start/stop Android system ONLY ONE TIME between each reboot due to Android init constraint.
    
    Reboot into recovery or bootloader (fastboot) is supported with new target files.
    eg: systemctl isolate recovery
    eg: systemctl isolate bootloader
    
    hybris-ready-bootanim permit to support a bootanimation based on QML. This is an optional dependency to hybris-ready.

--- GNULinux/Archlinux/PKGBUILDs/hybris-device-sony-nozomi/profile.hybris-device.sh
+++ GNULinux/Archlinux/PKGBUILDs/hybris-device-sony-nozomi/profile.hybris-device.sh
@@ -3,3 +3,26 @@
 
 source /etc/default/hybris-device
 
+####
+# Export useful variables
+
+export QT_QPA_EGLFS_PHYSICAL_WIDTH
+export QT_QPA_EGLFS_PHYSICAL_HEIGHT
+export QT_QPA_EGLFS_WIDTH
+export QT_QPA_EGLFS_HEIGHT
+export QT_QPA_EGLFS_DEPTH
+export QT_QPA_EGLFS_REFRESH_RATE
+
+export QT_QPA_PLATFORM
+
+export SURFACEFLINGER_DISPLAY
+export ANDROID_ROOT
+export KWIN_COMPOSE
+
+#export USB_IDVENDOR
+#export USB_IDPRODUCT
+#export USB_IPODUCT
+#export USB_ISERIAL
+#export USB_IMANUFACTURER
+#export USB_IFACE
+
