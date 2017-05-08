cff46cc49e2ebe15f3d8d7c3dcb66f5a48e2d0eb
mickybart <mickybart@pygoscelis.org>
2017-05-08 17:02:29 -0400

    Improvement to be near of libhybris upstream

--- Android/android_hybris/Android.mk
+++ Android/android_hybris/Android.mk
@@ -1,3 +1,8 @@
 ifeq ($(GNULINUX_SUPPORT),true)
-include $(all-subdir-makefiles)
+#include $(all-subdir-makefiles)
+MY_LOCAL_PATH := $(call my-dir)
+
+include $(MY_LOCAL_PATH)/libhybris/compat/surface_flinger/Android.mk
+include $(MY_LOCAL_PATH)/libhybris/compat/ui/Android.mk
+
 endif
--- Android/android_hybris/compat/Android.mk
+++
@@ -1,3 +0,0 @@
-hybris_compats := surface_flinger ui 
-
-include $(call all-named-subdir-makefiles,$(hybris_compats))
